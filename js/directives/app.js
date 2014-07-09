20dddc7c5282073602f61e4de9a9b04f39e4ab42
Joseph Buluran <joseph.buluran@gmail.com>
2014-07-09 20:22:29 +0800

    deleted HEAD merge comments.

--- js/directives/app.js
+++ js/directives/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 (function() {
 
 	var app = angular.module('mainDirective', []);
@@ -14,21 +13,4 @@
 		});
 
 	}]);
-
-=======
-(function(){
-	
-	var app= angular.module('mainDirective', []);
-	
-	app.controller('SearchController', ['$http', function($http){
-		var search = this;
-		search.mains = [];
-		$http.get('js/directives/phMobilePrefix.json').success(function(data){
-			search.mains = data;
-		});
-		
-	}]);
-	
-	
->>>>>>> gh-pages
 })();
